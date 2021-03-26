0a8301f3a72bb9d409d28d3df33a16ca4dceccd2
Sven Kalkbrenner <sven.kalkbrenner@gmail.com>
2021-03-26 08:40:33 +0100

    First commit

---
+++ src/webparts/wpDecisionPoint/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IWpDecisionPointWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'WpDecisionPointWebPartStrings' {
+  const strings: IWpDecisionPointWebPartStrings;
+  export = strings;
+}
